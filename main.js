102f499df97051637e1bf2564e4300ebdd8c9cce
Yuriybidjo <yuriybidjo@gmail.com>
2026-05-18 18:34:38 +0300

    Add main.js and initialized varibles

---
+++ main.js
@@ -0,0 +1,9 @@
+let userAgeValue = 20;
+let userNameNickname = 'Bond';
+
+console.log(userAgeValue, userNameNickname,);
+
+userAgeValue = 50;
+userNameNickname = 'Bill';
+
+console.log(userAgeValue, userNameNickname,);
