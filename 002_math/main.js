f03f513451cedfbef02e78fcff6d9a254c3c23b3
Yuriybidjo <yuriybidjo@gmail.com>
2026-05-18 20:35:07 +0300

    Created the folder 002_math for task 2

---
+++ 002_math/main.js
@@ -0,0 +1,45 @@
+// task 1
+let result1 = (5 + 3) * 2 - 7;
+let result2 = (10 / 2) ** 2;
+let result3 = 2 * (4 + 3) - (8 / 2);
+let result4 = (15 % 4) + 5;
+let result5 = 3 - (2 ** 4);
+
+console.log(result1); // 9
+console.log(result2); // 25
+console.log(result3); // 10
+console.log(result4); // 8 
+console.log(result5); // -13
+
+// task 2
+let calculateNumber1 = 55;
+let calculateNumber2 = 33;
+
+let calculateResult1 = calculateNumber1 + calculateNumber2;
+
+console.log(`Operation result: ${calculateResult1}`); // 88
+
+let calculateResult2 = calculateNumber1 - calculateNumber2;
+
+console.log(`Operation result: ${calculateResult2}`); // 22 
+
+let calculateResult3 = calculateNumber1 / calculateNumber2;
+
+console.log(`Operation result: ${calculateResult3}`); // 1.66
+
+let calculateResult4 = calculateNumber1 * calculateNumber2;
+
+console.log(`Operation result: ${calculateResult4}`); // 1815
+
+calculateNumber1 = calculateNumber1 % 2;
+
+console.log(`result 55 % 2 is: ${calculateNumber1}`); // 55 / 2 = 27.5 (27), 27 *2 = 54, 55 - 54 = 1.
+
+calculateNumber1 = calculateNumber1 % 3;
+
+console.log(`result 55 % 3 also is: ${calculateNumber1}`); // 55 / 3 = 18.3 (18), 18 * 3 = 54, 55 - 54 = 1.
+
+calculateNumber2 = calculateNumber2 ** 2;
+
+console.log(`result 33 ** 2 is: ${calculateNumber2}`); // 1089
+
