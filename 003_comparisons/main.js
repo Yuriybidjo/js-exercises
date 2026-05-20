89e2fbdeea951f6459e4ef41c8b0f8faab812aef
Yuriybidjo <yuriybidjo@gmail.com>
2026-05-20 18:45:33 +0300

    have done all tasks

--- 003_comparisons/main.js
+++ 003_comparisons/main.js
@@ -1,6 +1,5 @@
 // task 1
 
-
 let number1 = 33;
 let number2 = 25;
 
@@ -29,3 +28,35 @@ let evenNumber = anyNumber % 2 === 0;
 
 console.log(evenNumber);
 
+// Task 3
+
+let clientAge = 18;
+
+let enterIsAllow = Number(prompt(`Enter your age`, ''));
+
+if (enterIsAllow >= clientAge) {
+  console.log('Enter is allowed');
+} else {
+  console.log("Enter disallow");
+}
+
+
+// Task 4
+
+let goodsQuantity = Number(prompt(`Quantity of goods is:`, 1 )); 
+let goodsPrice = Number(prompt(`Price of goods is:`, 5));
+
+let totalPriceGoods = goodsPrice * goodsQuantity;
+
+console.log(`Total price of goods is: ${totalPriceGoods}`);
+
+
+// Task 5
+
+let productPrice = Number(prompt(`Enter price the product`, 0));
+
+let amountPerCentOff = 0.5; // 50%
+
+let productNewPrice = productPrice * amountPerCentOff;
+
+alert(`The price of the product with off 50%: ${productNewPrice}`);
