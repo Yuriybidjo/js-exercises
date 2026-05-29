26fa26e7f7f8312ab28c48db8dcf2ab87431f7b1
Yuriybidjo <yuriybidjo@gmail.com>
2026-05-29 12:52:53 +0300

    have done the task 2 and setting the debug panel in VS Code for debuging in terminal VS Code

--- 006_fn/main.js
+++ 006_fn/main.js
@@ -5,10 +5,10 @@ function addNumbers(a, b) {
 };
 
 function subtractNumbers(a, b) {
-  return a -b;
+  return a - b;
 };
 
-function multiplyNumbers(a, b){
+function multiplyNumbers(a, b) {
   return a * b;
 };
 
@@ -20,8 +20,8 @@ function divideNumbers(a, b) {
 };
 
 //Function with switch case
-function calculateValue(a, b, operator){
-  switch (operator){
+function calculateValue(a, b, operator) {
+  switch (operator) {
     case '+':
       return addNumbers(a, b);
     case '-':
@@ -34,9 +34,24 @@ function calculateValue(a, b, operator){
       return "Помилка: невідомий оператор!";
   }
 };
-console.log(calculateValue(10, 2, '+')); 
-console.log(calculateValue(9, 3, '-')); 
-console.log(calculateValue(2, 5, '*')); 
-console.log(calculateValue(5, 5, '/')); 
-console.log(calculateValue(10, 0, '/'));  
+console.log(calculateValue(10, 2, '+'));
+console.log(calculateValue(9, 3, '-'));
+console.log(calculateValue(2, 5, '*'));
+console.log(calculateValue(5, 5, '/'));
+console.log(calculateValue(10, 0, '/'));
 
+// Task 2 Параметри та колбеки: welcomeUser array callback hiUser console.log('Вітаю ${user}) для кожного юзер в масиві
+
+let users = ['Степан', 'Симон', 'Василь', 'Дмитро', 'Роман'];
+
+function welcomeUser(users, hiUser) {
+  for (let i = 0; i < users.length; i++) {
+    hiUser(users[i]);
+  }
+};
+
+let hiUser = (user) => {
+  console.log(`Вітаю ${user}`);
+};
+
+welcomeUser(users, hiUser);
