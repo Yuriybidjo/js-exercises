363c67b2f2034c4aac3adefbd34edccb9603e922
Yuriybidjo <yuriybidjo@gmail.com>
2026-06-28 16:50:26 +0300

    the task 3 have done

--- 015_event/script.js
+++ 015_event/script.js
@@ -10,11 +10,11 @@ function getRandomColor() {
 }
 
 generate_btn.addEventListener('click', () => {
-  const newColor = getRandomColor(); 
+  const newColor = getRandomColor();
 
-  text.style.color = newColor;          
-  text.style.borderColor = newColor;           
-  text.innerText = newColor; 
+  text.style.color = newColor;
+  text.style.borderColor = newColor;
+  text.innerText = newColor;
 });
 
 // 2. ondblclick
@@ -30,4 +30,30 @@ changeSize.ondblclick = function () {
   this.style.height = newHeight + 'px';
 };
 
+// 3. addEventListener і removeEventListener
 
+// Створити кнопку і текстовий елемент в якому розмість лічільник: 0, при кліку на кнопку в текстовому значенні повино збільшуватись значення на 1. Коли значення лічильника перевищує 10, кнопка перестає реагувати на кліки.
+
+const counter = document.querySelector('.counter');
+const btnIncrease = document.querySelector('.button.increase');
+
+let count = 0;
+
+counter.innerText = count;
+
+btnIncrease.addEventListener('click', () => {
+  count++;
+  counter.innerText = count;
+
+  if (count >= 10) {
+    btnIncrease.setAttribute('disabled', 'disabled');
+    btnIncrease.style.pointerEvents = 'none';
+  }
+  // або через функцію і css стилі 
+  // checkLimits();
+});
+
+// function checkLimits() {
+//   counter.innerText = count;
+//   btnIncrease.disabled = count >= 10;
+// }
